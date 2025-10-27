54424da5f1a949c4dd3e3e70c0a24e79c3b6b04c
johnazeh <92832525+johnazeh@users.noreply.github.com>
2025-10-27 11:49:31 -0700

    Add new tables and update DailyServerReport
    Added new tables for DatabaseInfo, Disk_Snapshot, JobInfo, ServerDiskUsage, ServerInfo, and Table_Snapshot. Updated DailyServerReport to include TotalDevDisksize and ChangeDevDisksize.

--- DBinventoryTables.sql
+++ DBinventoryTables.sql
@@ -1,7 +1,7 @@
 USE [DBA_Inventory]
 GO
 
-/****** Object:  Table [dbo].[DailyServerChangeLog]    Script Date: 10/21/2025 1:21:34 PM ******/
+/****** Object:  Table [dbo].[DailyServerChangeLog]    Script Date: 10/27/2025 11:47:10 AM ******/
 SET ANSI_NULLS ON
 GO
 
@@ -23,7 +23,7 @@ PRIMARY KEY CLUSTERED
 ) ON [PRIMARY]
 GO
 
-/****** Object:  Table [dbo].[DailyServerReport]    Script Date: 10/21/2025 1:21:34 PM ******/
+/****** Object:  Table [dbo].[DailyServerReport]    Script Date: 10/27/2025 11:47:10 AM ******/
 SET ANSI_NULLS ON
 GO
 
@@ -43,6 +43,8 @@ CREATE TABLE [dbo].[DailyServerReport](
 	[ChangeDevDB] [int] NULL,
 	[ChangeDevDbSize] [decimal](18, 2) NULL,
 	[ChangeDevServers] [int] NULL,
+	[TotalDevDisksize] [decimal](18, 2) NULL,
+	[ChangeDevDisksize] [decimal](18, 2) NULL,
 PRIMARY KEY CLUSTERED 
 (
 	[ReportID] ASC
@@ -50,7 +52,25 @@ PRIMARY KEY CLUSTERED
 ) ON [PRIMARY]
 GO
 
-/****** Object:  Table [dbo].[DB_Snapshot]    Script Date: 10/21/2025 1:21:34 PM ******/
+/****** Object:  Table [dbo].[DatabaseInfo]    Script Date: 10/27/2025 11:47:10 AM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[DatabaseInfo](
+	[DatabaseName] [nvarchar](128) NULL,
+	[ServerName] [nvarchar](128) NULL,
+	[TableCount] [int] NULL,
+	[SizeGB] [decimal](18, 2) NULL,
+	[GrowthPercent] [decimal](6, 2) NULL,
+	[CollectionDate] [datetime2](7) NULL,
+	[DatabaseNum] [int] NULL
+) ON [PRIMARY]
+GO
+
+/****** Object:  Table [dbo].[DB_Snapshot]    Script Date: 10/27/2025 11:47:10 AM ******/
 SET ANSI_NULLS ON
 GO
 
@@ -67,7 +87,24 @@ PRIMARY KEY CLUSTERED
 ) ON [PRIMARY]
 GO
 
-/****** Object:  Table [dbo].[Job_Snapshot]    Script Date: 10/21/2025 1:21:34 PM ******/
+/****** Object:  Table [dbo].[Disk_Snapshot]    Script Date: 10/27/2025 11:47:10 AM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[Disk_Snapshot](
+	[Drive] [char](1) NOT NULL,
+	[MB_Free] [decimal](18, 2) NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[Drive] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+/****** Object:  Table [dbo].[Job_Snapshot]    Script Date: 10/27/2025 11:47:10 AM ******/
 SET ANSI_NULLS ON
 GO
 
@@ -84,10 +121,87 @@ PRIMARY KEY CLUSTERED
 ) ON [PRIMARY]
 GO
 
+/****** Object:  Table [dbo].[JobInfo]    Script Date: 10/27/2025 11:47:10 AM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[JobInfo](
+	[ServerName] [nvarchar](128) NULL,
+	[JobName] [nvarchar](256) NULL,
+	[IsEnabled] [bit] NULL,
+	[CollectionDate] [datetime2](7) NULL,
+	[JOBNUMBER] [int] NULL
+) ON [PRIMARY]
+GO
+
+/****** Object:  Table [dbo].[ServerDiskUsage]    Script Date: 10/27/2025 11:47:10 AM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[ServerDiskUsage](
+	[ServerDiskUsageID] [int] IDENTITY(1,1) NOT NULL,
+	[ServerName] [nvarchar](128) NOT NULL,
+	[Drive] [char](1) NOT NULL,
+	[TotalSizeTB] [decimal](10, 2) NOT NULL,
+	[FreeGB] [decimal](10, 2) NOT NULL,
+	[FreePercent] [decimal](5, 2) NULL,
+	[CollectionDate] [datetime2](7) NOT NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[ServerDiskUsageID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+/****** Object:  Table [dbo].[ServerInfo]    Script Date: 10/27/2025 11:47:10 AM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[ServerInfo](
+	[ServerName] [nvarchar](128) NULL,
+	[SQLVersion] [nvarchar](128) NULL,
+	[Edition] [nvarchar](128) NULL,
+	[EngineEdition] [nvarchar](50) NULL,
+	[CollectionDate] [datetime2](7) NULL,
+	[AGStatus] [nvarchar](50) NULL,
+	[ServerNum] [int] NULL
+) ON [PRIMARY]
+GO
+
+/****** Object:  Table [dbo].[Table_Snapshot]    Script Date: 10/27/2025 11:47:10 AM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[Table_Snapshot](
+	[TableName] [nvarchar](256) NOT NULL,
+	[DatabaseName] [nvarchar](256) NOT NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[TableName] ASC,
+	[DatabaseName] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
 ALTER TABLE [dbo].[DailyServerChangeLog] ADD  DEFAULT (sysdatetime()) FOR [ChangeDate]
 GO
 
 ALTER TABLE [dbo].[DailyServerReport] ADD  DEFAULT (sysdatetime()) FOR [ReportDate]
+GO
+
+ALTER TABLE [dbo].[DatabaseInfo] ADD  DEFAULT (sysdatetime()) FOR [CollectionDate]
 GO
 
 ALTER TABLE [dbo].[DB_Snapshot] ADD  DEFAULT (sysdatetime()) FOR [CollectionDate]
@@ -95,5 +209,12 @@ GO
 
 ALTER TABLE [dbo].[Job_Snapshot] ADD  DEFAULT (sysdatetime()) FOR [CollectionDate]
 GO
+
+ALTER TABLE [dbo].[JobInfo] ADD  DEFAULT (sysdatetime()) FOR [CollectionDate]
+GO
+
+ALTER TABLE [dbo].[ServerInfo] ADD  DEFAULT (sysdatetime()) FOR [CollectionDate]
+GO
+
 
 
